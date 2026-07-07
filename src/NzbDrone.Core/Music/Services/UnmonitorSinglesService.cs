b8ad12fd092ba8356584f181693b8a89e8433e12
Stephen Rundell <stephen.rundell@gmail.com>
2026-07-07 15:25:04 -0500

    Fix UnmonitorSinglesService: add missing Instrumentation.Extensions using for ProgressInfo

--- src/NzbDrone.Core/Music/Services/UnmonitorSinglesService.cs
+++ src/NzbDrone.Core/Music/Services/UnmonitorSinglesService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NLog;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Common.Instrumentation.Extensions;
 using NzbDrone.Core.Messaging.Commands;
 using NzbDrone.Core.Music.Commands;
 
