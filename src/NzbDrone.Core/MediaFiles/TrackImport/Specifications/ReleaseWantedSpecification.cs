d415a17fa6d4d23735166e65c6bd44d2db24c525
srundell2006 <stephen.rundell@gmail.com>
2026-07-13 19:47:28 -0500

    Disable ReleaseWantedSpecification: always accept any identified release edition

--- src/NzbDrone.Core/MediaFiles/TrackImport/Specifications/ReleaseWantedSpecification.cs
+++ src/NzbDrone.Core/MediaFiles/TrackImport/Specifications/ReleaseWantedSpecification.cs
@@ -1,28 +1,16 @@
-using NLog;
 using NzbDrone.Core.DecisionEngine;
 using NzbDrone.Core.Download;
 using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.MediaFiles.TrackImport.Specifications
 {
+    // Disabled: always accept regardless of whether the specific release edition is
+    // monitored, so that any identified release can be imported into the library.
     public class ReleaseWantedSpecification : IImportDecisionEngineSpecification<LocalAlbumRelease>
     {
-        private readonly Logger _logger;
-
-        public ReleaseWantedSpecification(Logger logger)
-        {
-            _logger = logger;
-        }
-
         public Decision IsSatisfiedBy(LocalAlbumRelease item, DownloadClientItem downloadClientItem)
         {
-            if (item.AlbumRelease.Monitored || item.AlbumRelease.Album.Value.AnyReleaseOk)
-            {
-                return Decision.Accept();
-            }
-
-            _logger.Debug("AlbumRelease {0} was not requested", item);
-            return Decision.Reject("Album release not requested");
+            return Decision.Accept();
         }
     }
 }
